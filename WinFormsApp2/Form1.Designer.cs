2754d5eb9e6970272a3ddcc49abf13b0ff3b8017
Srujana Polisetti <srujana.polisetti@example.com>
2025-09-19 23:28:20 +0530

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            txtDigits = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            btnGenerate = new System.Windows.Forms.Button();
+            lblPrimeResult = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(123, 110);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(128, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Enter number of digits:";
+            // 
+            // txtDigits
+            // 
+            txtDigits.BackColor = System.Drawing.SystemColors.Window;
+            txtDigits.Location = new System.Drawing.Point(274, 107);
+            txtDigits.Name = "txtDigits";
+            txtDigits.Size = new System.Drawing.Size(164, 23);
+            txtDigits.TabIndex = 1;
+            txtDigits.TextChanged += txtDigits_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(123, 161);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(91, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Prime Number :";
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            btnGenerate.Location = new System.Drawing.Point(274, 251);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new System.Drawing.Size(164, 23);
+            btnGenerate.TabIndex = 3;
+            btnGenerate.Text = "Generate Prime";
+            btnGenerate.UseVisualStyleBackColor = false;
+            btnGenerate.Click += button1_Click;
+            // 
+            // lblPrimeResult
+            // 
+            lblPrimeResult.AutoSize = true;
+            lblPrimeResult.Location = new System.Drawing.Point(274, 161);
+            lblPrimeResult.Name = "lblPrimeResult";
+            lblPrimeResult.Size = new System.Drawing.Size(38, 15);
+            lblPrimeResult.TabIndex = 4;
+            lblPrimeResult.Text = "prime";
+            lblPrimeResult.Click += lblPrimeResult_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.InactiveCaption;
+            ClientSize = new System.Drawing.Size(641, 337);
+            Controls.Add(lblPrimeResult);
+            Controls.Add(btnGenerate);
+            Controls.Add(label2);
+            Controls.Add(txtDigits);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDigits;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblPrimeResult;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Changed to PascalCase
+        {
+            try
+            {
+                int numDigits = int.Parse(txtDigits.Text);
+
+                if (numDigits < 1)
+                {
+                    MessageBox.Show("Please enter a valid number of digits.");
+                    return;
+                }
+
+                BigInteger prime = GenerateRandomPrime(numDigits);
+                lblPrimeResult.Text = prime.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private BigInteger GenerateRandomPrime(int numDigits)
+        {
+            Random random = new Random();
+            BigInteger lowerBound = BigInteger.Pow(10, numDigits - 1);
+            BigInteger upperBound = BigInteger.Pow(10, numDigits) - 1;
+            BigInteger candidate;
+
+            do
+            {
+                candidate = GenerateRandomNumberBetween(lowerBound, upperBound, random);
+            }
+            while (!IsPrime(candidate));
+
+            return candidate;
+        }
+
+        private static BigInteger Sqrt(BigInteger number)
+        {
+            if (number == 0) return 0;
+            if (number > 0)
+            {
+                BigInteger n = number / 2 + 1;
+                BigInteger n1 = (n + number / n) / 2;
+                while (n1 < n)
+                {
+                    n = n1;
+                    n1 = (n + number / n) / 2;
+                }
+                return n;
+            }
+
+            // Handle negative numbers (though primes cannot be negative, it's good to avoid errors)
+            throw new ArithmeticException("Cannot calculate the square root of a negative number.");
+        }
+
+        private BigInteger GenerateRandomNumberBetween(BigInteger lower, BigInteger upper, Random random)
+        {
+            byte[] bytes = upper.ToByteArray();
+            BigInteger randomNumber;
+
+            do
+            {
+                random.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= (byte)0x7F; // Force positive number
+                randomNumber = new BigInteger(bytes);
+            }
+            while (randomNumber < lower || randomNumber > upper);
+
+            return randomNumber;
+        }
+
+        private bool IsPrime(BigInteger number)
+        {
+            if (number < 2) return false;
+
+            BigInteger sqrt = Sqrt(number); // Use the custom Sqrt method
+
+            for (BigInteger i = 2; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void lblPrimeResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDigits_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
